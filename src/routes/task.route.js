05ee18c12bfc429248e0707b3efe2e107422eb46
Thiago Mota <th.araujomota@gmail.com>
2023-09-06 19:59:09 -0300

    implements DELETE/:id endpoint

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -5,6 +5,7 @@ const tasksRouter = express.Router();
 
 tasksRouter.get('/', taskController.findAllTasks);
 tasksRouter.get('/:id', taskController.getTaskById);
+tasksRouter.delete('/:id', taskController.deleteTaskById);
 tasksRouter.post('/', taskController.createTask);
 
 module.exports = tasksRouter;
