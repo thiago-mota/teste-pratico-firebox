75e1df4779caff645e0e9c66f94f45e8eeed20f7
Thiago Mota <th.araujomota@gmail.com>
2023-09-06 19:47:13 -0300

    adds getTaskById function on tasks controller

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -3,7 +3,7 @@ const { taskService } = require('../services');
 
 const findAllTasks = async (_request, response) => {
 	const allTasks = await taskService.findAllTasks();
-	return response.status(StatusCodes.OK).json(allTasks);
+	return response.status(StatusCodes.OK).json({ message: allTasks });
 };
 
 const createTask = async (request, response) => {
@@ -13,13 +13,20 @@ const createTask = async (request, response) => {
 		description,
 		data,
 	);
-
 	return response
 		.status(StatusCodes.CREATED)
 		.json({ message: lastInsertedTask });
 };
 
+const getTaskById = async (request, response) => {
+	const { id } = request.params;
+	const task = await taskService.getTaskById(id);
+
+	return response.status(StatusCodes.OK).json({ message: task });
+};
+
 module.exports = {
 	findAllTasks,
 	createTask,
+	getTaskById,
 };
