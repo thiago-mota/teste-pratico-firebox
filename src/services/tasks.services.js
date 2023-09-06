bfb23f44071797af92ceac4baa6b0b8fe756e94b
Thiago Mota <th.araujomota@gmail.com>
2023-09-06 18:12:23 -0300

    adds createTask function on task service

--- src/services/tasks.services.js
+++ src/services/tasks.services.js
@@ -5,6 +5,12 @@ const findAllTasks = async () => {
 	return tasks;
 };
 
+const createTask = async (name, description, data) => {
+	const newTask = await taskModel.createTask(name, description, data);
+	return newTask;
+};
+
 module.exports = {
 	findAllTasks,
+	createTask,
 };
