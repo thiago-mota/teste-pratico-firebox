d77be2930d12852896ff840cdb544dcc37c785eb
Thiago Mota <th.araujomota@gmail.com>
2023-09-06 19:47:40 -0300

    implements GET/:id endpoint

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -4,6 +4,7 @@ const { taskController } = require('../controllers');
 const tasksRouter = express.Router();
 
 tasksRouter.get('/', taskController.findAllTasks);
+tasksRouter.get('/:id', taskController.getTaskById);
 tasksRouter.post('/', taskController.createTask);
 
 module.exports = tasksRouter;
