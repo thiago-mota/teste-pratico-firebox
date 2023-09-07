b8679ae64eccc1b9679b8a461ecefb9b6ea71462
Thiago Mota <th.araujomota@gmail.com>
2023-09-07 02:35:52 -0300

    adds mocks for all tasks

---
+++ src/helpers/mocks/tasks.mocks.js
@@ -0,0 +1,26 @@
+const expectedTasks = {
+	tasks: [
+		{
+			id: 49,
+			name: 'Business Meeting',
+			description: 'Business meeting with XPTO employees',
+			data: '2023-09-06T01:01:01.000Z',
+		},
+		{
+			id: 50,
+			name: 'Medical appointment',
+			description: 'Tomorrow morning, Dr. Dohn Doe',
+			data: '2023-09-06T02:02:02.000Z',
+		},
+		{
+			id: 51,
+			name: 'Pvzinho b-day',
+			description: 'yeee lets party hard baby',
+			data: '2023-09-06T04:04:04.000Z',
+		},
+	],
+};
+
+module.exports = {
+	expectedTasks,
+};
