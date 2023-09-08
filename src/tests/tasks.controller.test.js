3e3745065a1247c4e65e3f27a492ab5d486c2c78
Thiago Mota <th.araujomota@gmail.com>
2023-09-08 00:40:13 -0300

    adds test for POST/
    refac expects from GET/ and GET/:id tests

--- src/tests/tasks.controller.test.js
+++ src/tests/tasks.controller.test.js
@@ -4,8 +4,13 @@ const AxiosMockAdapter = require('axios-mock-adapter');
 const {
 	expectedTasks,
 	expectedSingleTask,
+	newTask,
 } = require('../helpers/mocks/tasks.mocks');
-const { getAllTasks, getSingleTask } = require('../helpers/axios/axios.fetchs');
+const {
+	getAllTasks,
+	getSingleTask,
+	createTask,
+} = require('../helpers/axios/axios.fetchs');
 
 const mock = new AxiosMockAdapter(axios);
 
@@ -22,24 +27,43 @@ describe('Task Controller', () => {
 
 		expect(status).toBe(200);
 		expect(tasks).toHaveLength(3);
-		expect(tasks[0]).toHaveProperty('id');
-		expect(tasks[0]).toHaveProperty('name');
-		expect(tasks[0]).toHaveProperty('description');
-		expect(tasks[0]).toHaveProperty('data');
+		expect(tasks[2]).toMatchObject({
+			id: 3,
+			name: 'Pvzinho b-day',
+			description: 'yeee lets party hard baby',
+			data: '2023-09-06T04:04:04.000Z',
+		});
 	});
 
 	it('GET/:id -> should return status 200 and have correct tasks properties', async () => {
 		mock.onGet('http://localhost:3001/1').reply(200, expectedSingleTask);
 		const response = await getSingleTask();
-		console.log(response);
 
 		const { status, data } = response;
 		const { tasks } = data;
 
 		expect(status).toBe(200);
-		expect(tasks[0]).toHaveProperty('id');
-		expect(tasks[0]).toHaveProperty('name');
-		expect(tasks[0]).toHaveProperty('description');
-		expect(tasks[0]).toHaveProperty('data');
+		expect(tasks[0]).toMatchObject({
+			id: 1,
+			name: 'Business Meeting',
+			description: 'Business meeting with XPTO employees',
+			data: '2023-09-06T01:01:01.000Z',
+		});
+	});
+
+	it('POST/ -> should return status 201 and create a new task', async () => {
+		mock.onPost('http://localhost:3001/').reply(201, newTask);
+		const response = await createTask();
+
+		const { status, data } = response;
+		const { tasks } = data;
+
+		expect(status).toBe(201);
+		expect(tasks[0]).toMatchObject({
+			id: 4,
+			name: 'Zoo day with the Kids',
+			description: 'Remember to bring water and sun protector',
+			data: '2023-09-15T08:30:35.000Z',
+		});
 	});
 });
