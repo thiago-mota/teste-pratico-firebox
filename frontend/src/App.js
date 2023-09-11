4a3ea707e34fcd1e8c2271b538fd8a6afb2c6cb0
Thiago Mota <th.araujomota@gmail.com>
2023-09-11 01:29:12 -0300

    corrige logica
    passa fetchData e estado para o App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import Header from './components/Header';
 import Form from './components/Form';
 import Tasks from './components/Tasks';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		fetchData().then((data) => setTasks(data));
+	}, []);
+
+	const fetchData = async () => {
+		const result = await axios.get('http://localhost:3001');
+		const { tasks } = result.data;
+		return tasks;
+	};
+
 	return (
 		<div className='bg-neutral-800 min-h-screen'>
 			<Header />
 			<Form />
-			<Tasks />
+			<Tasks
+				tasks={tasks}
+				setTasks={setTasks}
+			/>
 		</div>
 	);
 };
