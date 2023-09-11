563dd13aec77a4fb4562e068db1cf0ff84b793ac
Thiago Mota <th.araujomota@gmail.com>
2023-09-11 01:28:34 -0300

    remove gambiarra com estado e utiliza requisicoes a api
    corrige bugs
    
    altera condicoes de exibicao do botao de deletar

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import { Trash } from '@phosphor-icons/react';
 moment.locale('pt-br');
 
-const Tasks = () => {
-	const [tasks, setTasks] = useState([]);
-
-	useEffect(() => {
-		const fetchTasks = async () => {
-			const localTasks = JSON.parse(localStorage.getItem('tasks'));
-
-			if (localTasks) {
-				setTasks(localTasks);
-			} else {
-				const result = await axios.get('http://localhost:3001');
-				const { tasks } = result.data;
-
-				const addsCompletedKey = tasks.map((task) => ({
-					...task,
-					completed: false,
-				}));
-				setTasks(addsCompletedKey);
-				localStorage.setItem('tasks', JSON.stringify(addsCompletedKey));
-			}
+const Tasks = ({ tasks, setTasks }) => {
+	const setCompleted = async (task) => {
+		const updatedTask = {
+			...task,
+			status: !task.status,
 		};
+		const { name, description, data, status } = updatedTask;
 
-		fetchTasks();
-	}, []);
+		await axios.put(`http://localhost:3001/${task.id}`, {
+			name,
+			description,
+			data,
+			status,
+		});
 
-	const setCompleted = (index) => {
-		const updatedTasks = [...tasks];
-		updatedTasks[index].completed = !updatedTasks[index].completed;
-		setTasks(updatedTasks);
+		setTasks(
+			tasks.map((element) => {
+				if (element.id === task.id) {
+					return updatedTask;
+				}
+				return element;
+			}),
+		);
+	};
 
-		localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+	const deleteTask = async (id) => {
+		await axios.delete(`http://localhost:3001/${id}`);
+		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
 	return (
@@ -45,22 +41,20 @@ const Tasks = () => {
 				{tasks.map((task, index) => (
 					<div
 						key={index}
-						onClick={() => setCompleted(index)}
-						style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+						onClick={() => setCompleted(task)}
+						style={{ textDecoration: task.status ? 'line-through' : 'none' }}
 						className='text-white'
 					>
-						{task.name}: {task.description}, {moment(task.data).format('LLLL')}
-						{task.completed && (
-							<button
-								type='button'
-								id='deleteTask'
-								name='deleteTask'
-								className='border-black bg-black rounded-xl'
-								onClick={() => console.log('xablau')}
-							>
-								<Trash size={16} />
-							</button>
-						)}
+						{task.name}: {task.description}, {moment(task.data).format('llll')}
+						<button
+							type='button'
+							id='deleteTask'
+							name='deleteTask'
+							className='border-black bg-black rounded-xl'
+							onClick={() => deleteTask(task.id)}
+						>
+							<Trash size={16} />
+						</button>
 					</div>
 				))}
 			</div>
